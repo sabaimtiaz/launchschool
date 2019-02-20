46ef633b5caf64814391a35839d2f0bc5d2ab880
sabaimtiaz <saba.imtiaz@gmail.com>
2019-02-20 08:35:10 +0500

    adding pseudocode and rubocop assignments

---
+++ RB101/lesson_2/pseudocode.rb
@@ -0,0 +1,45 @@
+#a method that returns the sum of two integers
+#Casual:
+#gets input from user or variables from the method
+#adds two numbers
+#shows the sum
+
+#Formal:
+#START the program
+#Define method
+#Either provide and READ two variables or GET integer 1 and integer 2 from user
+#Perform operation
+#END program
+#PRINT sum of integers
+
+#a method that takes an array of strings, and returns a string that is all those strings concatenated together 
+#Given an array of strings
+#Takes array of strings
+#A loop that reads each string in the array, and outputs each string 
+#Then add all the strings together and set it as a new string
+#PRINTs the new string 
+
+#Given an array of strings
+#START the program
+#Run loop until array is emptied so WHILE the iterator is the length of the array
+#READ array, take each element of the array
+#output each string SET new string 
+#add each string
+#PRINT strings added together as a new string
+#END
+
+#a method that takes an array of integers, and returns a new array with every other element 
+#Takes an array of integers
+#Reads 'every other element' - so every third element (1,3,5, 7..)
+#Returns a new array with every other element
+
+#START the program
+#Run a loop that goes through the array index [each?]
+#SET index value to start at 1
+#SET iterator = 2 [So that it skips over] || set index to odd with a condition?
+#Loops through index and READ array value at index  
+#SETS new array 
+#PRINT integers at odd index values as a new array
+#END
+
+
