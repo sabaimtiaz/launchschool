8c39ec193e441e2b92326d06248c95e8e3c2033d
sabaimtiaz <saba.imtiaz@gmail.com>
2019-02-21 17:53:21 +0500

    Add keyword arguments exercise

---
+++ RB101/small_problems/documentation_again/keyword-args.rb
@@ -0,0 +1,4 @@
+5.step(to: 10, by: 3) { |value| puts value}
+# from documentation - step(by: step, to: limit) {|i| block } --> self
+# step(by: step, to: limit) -> enumerator
+# 4.step(to: 16, by: 2) { |value| puts value}
