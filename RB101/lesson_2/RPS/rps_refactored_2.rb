c901b426c70a35a0d3e1ca07a77e1e680ef1165d
sabaimtiaz <saba.imtiaz@gmail.com>
2019-03-27 11:57:12 +0500

    RPS Refactored - changes after review #2

--- RB101/lesson_2/RPS/rps_refactored_2.rb
+++ RB101/lesson_2/RPS/rps_refactored_2.rb
@@ -28,43 +28,57 @@ def determine_winner?(player, computer)
 end
 
 def display_results(returned_val, computer_choice)
-  if determine_winner?(returned_val, computer_choice)
+  winner = determine_winner?(returned_val, computer_choice)
+  if returned_val == computer_choice
+    prompt("It's a tie")
+  elsif winner == true
     prompt('You win!')
-  elsif determine_winner?(computer_choice, returned_val)
+  else
     prompt('You lose!')
-  elsif returned_val == computer_choice
-    puts "It's a tie."
   end
 end
 
-loop do
-  puts "Welcome to Rock Paper Scissors Spock Lizard!"
-  CHOICES.each { |key, value| puts "Enter #{value} or #{key} for #{value}" }
-  input = gets.chomp
-  returned_val = convert_choice(input)
+def valid_choice?(choice)
+  CHOICES.to_a.flatten.include?(choice)
+end
 
-  system('clear')
-    if CHOICES.to_a.flatten.include?(input)
-    prompt("You chose #{returned_val}.")
-  else
-    puts "This is invalid."
-    break
+loop do
+  input = ''
+
+  loop do
+    puts "Welcome to Rock Paper Scissors Spock Lizard!"
+    CHOICES.each { |key, value| puts "Enter #{value} or #{key} for #{value}" }
+    input = gets.chomp
+    if valid_choice?(input)
+      break
+    else
+      prompt('This is invalid.')
+      prompt('Do you want to play again?')
+      prompt('Enter y to continue. Press any other key to exit:')
+      answer = gets.chomp
+      break unless answer.downcase.start_with?('y')
+      system('clear')
+    end
   end
 
+  returned_val = convert_choice(input)
   computer_choice = CHOICES.values.sample
+  prompt("You chose #{returned_val}.")
   prompt("The computer chose #{computer_choice}.")
+
+  winner = determine_winner?(returned_val, computer_choice)
 
   display_results(returned_val, computer_choice)
 
-  if determine_winner?(returned_val, computer_choice)
+  if winner == true
     player_score += 1
     prompt("You scored 1.")
-  elsif determine_winner?(computer_choice, returned_val)
+  elsif winner == false
     computer_score += 1
     prompt("The computer scored 1.")
   end
 
-  determine_winner?(returned_val, computer_choice)
+  system('clear')
   prompt("Your total score is #{player_score}.")
   prompt("The computer's score is #{computer_score}.")
 
@@ -72,9 +86,11 @@ loop do
     prompt("5 wins! Game over!")
     break
   end
-  prompt('Do you want to play again? Enter y or any other key to exit:')
+  prompt('Do you want to play again?')
+  prompt('Enter y to continue. Press any other key to exit:')
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
   system('clear')
 end
+
 prompt('Thank you for playing. Live long and prosper!')
