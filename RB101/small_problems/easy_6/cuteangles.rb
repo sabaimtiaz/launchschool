65e952531fc4d42ce439aa8b01cf618d7e2475ef
sabaimtiaz <saba.imtiaz@gmail.com>
2019-04-23 10:17:49 +0500

    With LS solution

--- RB101/small_problems/easy_6/cuteangles.rb
+++ RB101/small_problems/easy_6/cuteangles.rb
@@ -9,14 +9,33 @@
 # A degree has 60 minutes, 
 # while a minute has 60 seconds.
 
-DEGREE_SYM = "\xC2\xB0"
+DEGREE = "\xC2\xB0"
 
 def dms(degree)
   degree_decimal = degree - degree.floor
   minutes = degree_decimal * 60
   degree_remaining = minutes - minutes.floor
   seconds = degree_remaining * 60
-  puts format(%(#{degree.round}#{DEGREE_SYM}%02d'%02d"), minutes.floor, seconds.floor)
+  puts format(%(#{degree.round}#{DEGREE}%02d'%02d"), minutes.floor, seconds.floor)
+end
+dms(121.35)
+dms(30)
+dms(76.73)
+dms(93.034773)
+dms(0)
+dms(360)
+
+# LS solution
+
+MINUTES_PER_DEGREE = 60
+SECONDS_PER_MINUTE = 60
+SECONDS_PER_DEGREE = MINUTES_PER_DEGREE * SECONDS_PER_MINUTE
+
+def dms(degrees_float)
+  total_seconds = (degrees_float * SECONDS_PER_DEGREE).round
+  degrees, remaining_seconds = total_seconds.divmod(SECONDS_PER_DEGREE)
+  minutes, seconds = remaining_seconds.divmod(SECONDS_PER_MINUTE)
+  puts format(%(#{degrees}#{DEGREE}%02d'%02d"), minutes, seconds)
 end
 dms(121.35)
 dms(30)
