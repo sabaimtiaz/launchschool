b01f9170c2ec9c2c38dc6ec6bccdcd63d88b5fa1
sabaimtiaz <saba.imtiaz@gmail.com>
2019-05-01 12:30:08 +0500

    Combining arrays exercise

---
+++ RB101/small_problems/easy_6/combining.rb
@@ -0,0 +1,15 @@
+
+
+def merge(arr1, arr2)
+  arr1 << arr2
+  arr1.flatten!.uniq!
+  arr1
+end
+merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]
+
+
+# LS solution
+def merge(array_1, array_2)
+  array_1 | array_2
+end
+merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]
