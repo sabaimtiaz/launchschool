16fb0ea302627c7a0bdd05253c644fdc7c343e7b
sabaimtiaz <saba.imtiaz@gmail.com>
2019-05-08 10:07:12 +0500

    Staggered case 2

---
+++ RB101/small_problems/easy_7/stag_case2.rb
@@ -0,0 +1,20 @@
+def staggered_case(string)
+  result = ''
+  need_upper = true
+  string.chars.each do |char|
+    if char =~ /[a-z]/i # i flag - ignore case
+      if need_upper
+        result += char.upcase
+      else
+        result += char.downcase
+      end
+      need_upper = !need_upper
+    else
+      result += char
+    end
+  end
+  result
+end
+p staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
+p staggered_case('ALL CAPS') == 'AlL cApS'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
