5b2e76be82232b8da35d3d6a43f195ef5f357944
sabaimtiaz <saba.imtiaz@gmail.com>
2019-05-13 09:18:15 +0500

    Uppercase exercise from Easy 9

---
+++ RB101/small_problems/easy_9/uppercase.rb
@@ -0,0 +1,13 @@
+def uppercase?(string)
+  if string == string.upcase
+    true
+  else 
+    false
+  end
+end
+p uppercase?('t') == false
+p uppercase?('T') == true
+p uppercase?('Four Score') == false
+p uppercase?('FOUR SCORE') == true
+p uppercase?('4SCORE!') == true
+p uppercase?('') == true
