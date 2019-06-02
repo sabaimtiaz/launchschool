780713786f8efd2a7122f0807411a0508aed9b12
sabaimtiaz <saba.imtiaz@gmail.com>
2019-06-02 10:00:31 +0500

    Lights

--- RB101/small_problems/medium_1/lights.rb
+++ RB101/small_problems/medium_1/lights.rb
@@ -1,17 +1,28 @@
-# toggle switches from 1 - n
-# round 1: every light is turned on
-# first every light
-# 2 = every second light
-# every third
+# toggle switches
+# the switches are first turned off
+# in round one we switch on all the switches
+# in round two, we change the switches that are multiples of 2
+# in subsequent rounds, change the switches in multiples of 3, 4, 5....
+# need to store the 'on/off' status
+# data structure - use an array of 0s and switch them to 1s
+# algorithm:
+# initialise the data structure - array of 0s
+# loop
+# iterate through the 0s array and "switch" the 0s to 1s
+# subsequent iterations: loop through this array and toggle 1s to 0s and 0s to 1s
+# possible second method to just switch on/off
+# returns the position of the "on" switches as an array which we know by doing index + 1
+# need two iterators
+# iterator 1 = index + 1
+# iterator 2 = n repetitions
 
-# starting / round one
-lights = [0, 0, 0, 0, 0]
-iterator = 0
-iterator += 1
-newarr = []
-5.times do |light|  
-  newarr << light += iterator
+
+lights = [0, 1, 0, 1, 0]
+
+def toggle(switches)
+  switches.map do |switch| 
+    switch == 0 ? 1 : 0 
+  end
 end
 
-p newarr
-# round two
+p toggle(lights) 
