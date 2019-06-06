98946021a60dfffcbcc8733edd3c27b8a6a0638f
sabaimtiaz <saba.imtiaz@gmail.com>
2019-06-06 16:50:27 +0500

    Complete diamonds exercise

--- RB101/lesson_6/ttt_bonus_reboot.rb
+++ RB101/lesson_6/ttt_bonus_reboot.rb
@@ -13,7 +13,8 @@ RISK_LINES =    [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                 [[2, 3, 1], [5, 6, 4], [8, 9, 7]] + 
                 [[4, 7, 1], [5, 8, 2], [6, 9, 3]] +
                 [[5, 9, 1], [5, 7, 3]] +
-                [[7, 5, 3], [3, 5, 7]]
+                [[3, 5, 7], [7, 5, 3]] +
+                [[1, 9, 5], [1, 5, 9]]
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -77,6 +78,7 @@ def detect_risk_location(brd)
     first_val = brd[line[0]]
     second_val = brd[line[1]]
     third_val = brd[line[2]]
+ #   binding.pry
     if first_val == "X" && second_val == "X" && third_val == " "
       return line[2]
     elsif first_val == " " && second_val == "X" && third_val == "X"
@@ -87,9 +89,26 @@ def detect_risk_location(brd)
   end
 end
 
+def detect_winning_move(brd)
+  RISK_LINES.each do |line|
+    first_val = brd[line[0]]
+    second_val = brd[line[1]]
+    third_val = brd[line[2]]
+    if first_val == "O" && second_val == "O" && third_val == " "
+      return line[0]
+    else
+      return nil
+    end
+  end
+end
+
 def computer_places_piece!(brd)
   if detect_risk_location(brd) != nil
     square = detect_risk_location(brd)
+    brd[square] = COMPUTER_MARKER
+  #  binding.pry
+  elsif detect_winner(brd) != nil
+    square = detect_winning_move(brd)
     brd[square] = COMPUTER_MARKER
   else
     square = empty_squares(brd).sample
---
+++ RB101/small_problems/medium_1/diamonds.rb
@@ -0,0 +1,74 @@
+require 'pry'
+def diamond(n)
+  iterator_1 = n
+  iterator_star = 1
+  space = ' '
+  n.times do
+    break if iterator_star >= n
+    puts (space * iterator_1) + ("*" * iterator_star)
+    iterator_1 -= 1
+    iterator_star += 2
+   end
+  n.times do
+    puts (space * iterator_1) + ("*" * n)
+    iterator_1 += 1
+    n -= 2
+    break if n < 1
+  end
+end
+diamond(3)
+
+# LS solution
+def print_row(grid_size, distance_from_center)
+  number_of_stars = grid_size - 2 * distance_from_center
+  stars = '*' * number_of_stars
+  puts stars.center(grid_size)
+end
+
+def diamond(grid_size)
+  max_distance = (grid_size - 1) / 2
+  max_distance.downto(0) { |distance| print_row(grid_size, distance)}
+  1.upto(max_distance)   { |distance| print_row(grid_size, distance)}
+end
+diamond(3)
+
+# ls user solutiion
+def diamond(n)
+  star = "*"
+  top = n/2
+  bottom = n/2 + 1
+  top.times do
+    puts star.center(n)
+    star += "**"
+  end
+  bottom.times do |i|
+    star = '*' * (n - i * 2)
+    puts star.center(n)
+  end
+end
+diamond(5)
+
+puts "----------- "
+
+#reworked
+def diamond(n)
+  iterator_1 = n
+  iterator_star = 1
+  space = ' '
+  center_val = n
+  # top half
+  n.times do
+    break if iterator_star >= n
+    puts ("*" * iterator_star).center(center_val)
+    iterator_1 -= 1
+    iterator_star += 2
+   end
+  # bottom half
+  n.times do
+    break if n < 1
+    puts ("*" * n).center(center_val)
+    iterator_1 += 1
+    n -= 2
+  end
+end
+diamond(3)
