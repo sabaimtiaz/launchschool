3fdce655508cb8403cc149a67fd78344bd72e114
sabaimtiaz <saba.imtiaz@gmail.com>
2019-06-08 09:34:43 +0500

    Complete diamonds exercise

--- RB101/small_problems/medium_1/diamonds.rb
+++ RB101/small_problems/medium_1/diamonds.rb
@@ -16,7 +16,7 @@ def diamond(n)
     break if n < 1
   end
 end
-diamond(3)
+# diamond(3)
 
 # LS solution
 def print_row(grid_size, distance_from_center)
@@ -30,7 +30,7 @@ def diamond(grid_size)
   max_distance.downto(0) { |distance| print_row(grid_size, distance)}
   1.upto(max_distance)   { |distance| print_row(grid_size, distance)}
 end
-diamond(3)
+# diamond(3)
 
 # ls user solutiion
 def diamond(n)
@@ -46,29 +46,51 @@ def diamond(n)
     puts star.center(n)
   end
 end
-diamond(5)
 
 puts "----------- "
 
 #reworked
 def diamond(n)
-  iterator_1 = n
   iterator_star = 1
-  space = ' '
   center_val = n
   # top half
   n.times do
     break if iterator_star >= n
     puts ("*" * iterator_star).center(center_val)
-    iterator_1 -= 1
     iterator_star += 2
    end
   # bottom half
   n.times do
     break if n < 1
     puts ("*" * n).center(center_val)
-    iterator_1 += 1
     n -= 2
   end
 end
-diamond(3)
+diamond(5)
+
+puts "-----------"
+
+# further exploration
+
+def diamond(n)
+  iterator_star = 1
+  center_val = n
+  star = "*"
+  space = " "
+  # top half
+  n.times do
+    break if iterator_star >= (n/2)
+    puts star.center(center_val) if iterator_star == 1
+    puts (star + (space * iterator_star) + star).center(center_val) 
+    iterator_star += 2
+   end
+  # bottom half
+  n.times do
+    break if n/2 < 1 || iterator_star < 1
+    puts (star + (space * iterator_star) + star).center(center_val)
+    n -= 2
+    puts star.center(center_val) if iterator_star <= 1
+    iterator_star -= 2
+  end
+end
+diamond(5)
