0d0bbe6003a0f5c649cfd6af8fbe878674ee24c4
sabaimtiaz <saba.imtiaz@gmail.com>
2019-07-12 21:07:37 +0500

    Complete word digit exercise

--- RB101/small_problems/medium_1/word_digit.rb
+++ RB101/small_problems/medium_1/word_digit.rb
@@ -1,18 +1,15 @@
-# Write a method that takes a sentence string as input, 
-# and returns the same string with any sequence of the words 
-# 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' 
-# converted to a string of digits.
-
-# word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. 
-
 DIGITS_TO_WORDS = {"one"=>"1", "two"=>"2", "three"=>"3", "four"=>"4", "five"=>"5", "six"=>"6", "seven"=>"7", "eight"=>"8", "nine"=>"9"}
 
 def word_to_digit(input)
-  DIGITS_TO_WORDS.keys.each do |word|
-    input.gsub!(/\b#{word}\b/, DIGITS_TO_WORDS[word])
-    input.gsub!(/[[:blank:]]/,"")
-  end
-input
+    output = ''
+    DIGITS_TO_WORDS.keys.each do |word|
+       input.gsub!(/#{word}/, DIGITS_TO_WORDS[word])
+    end
+    input.to_s.split(' ').each do |word|
+        output << word + " " if /[a-z]/.match(word)
+        output << word if DIGITS_TO_WORDS.values.include?(word)
+    end
+    output
 end
-p word_to_digit("please call me at one five five")
+p word_to_digit("pls call a one two five nine")
 
