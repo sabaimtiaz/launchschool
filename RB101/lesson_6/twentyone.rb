32dfab14a0318606faed23fd6d57b11caf4411aa
sabaimtiaz <saba.imtiaz@gmail.com>
2019-07-27 10:50:27 +0500

    Working on gameplay

--- RB101/lesson_6/twentyone.rb
+++ RB101/lesson_6/twentyone.rb
@@ -1,9 +1,5 @@
-# DECK = {one: "1", two: "2", three: "3", four: "4", five: "5",
-#         six: "6", seven: "7", eight: "8", nine: "9", ten: "10",
-#         jack: "0", queen: "0", king: "0", ace: "0"}
-require 'pry'
-# DECK = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "king", "queen", "ace"]
-DECK = [5, 5, "ace"]
+
+DECK = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "king", "queen", "ace"]
 suits = %w[hearts diamonds clubs spades]
 num_of_cards = 52
 
@@ -12,17 +8,19 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
-cards = []
-2.times do
-  cards << DECK.sample(1) + suits.sample(1)
+def generate(gamecards)
+  cards = []
+  2.times do
+    cards << DECK.sample(1) + suits.sample(1)
+  end
 end
 
-def total(cards)
-  values = cards.map { |card| card[0] }
+def total(gamecards)
+  values = gamecards.map { |card| card[0] }
   sum = 0
   values.each do |value|
     if value == "jack" || value == "king" || value == "queen"
-      sum += 10
+      sum += 10 if sum != 21
     else
       sum += value.to_i
     end
@@ -30,35 +28,36 @@ def total(cards)
       sum += 1
     elsif 
       value == "ace" && sum < 20
-        sum += 11
+        sum += 11 if sum != 21
       end
   end
   sum
 end
-total(cards)
 
-#  gameplay
-# initialise game
 prompt "Welcome to Twenty One!"
 
 # deal cards
 player_cards = []
   2.times do
-    player_cards << DECK.sample(1) + suits.sample(1)
+    player_cards << DECK.sample(1) + suits.sample(1) 
   end
 player_cards
-prompt "You have #{player_cards} and the sum is #{total(player_cards)}"
+prompt "You have #{player_cards} and the total is #{total(player_cards)}"
 
 dealer_cards = []
   2.times do
-    dealer_cards << DECK.sample(1) + suits.sample(1)
+    dealer_cards << DECK.sample(1) + suits.sample(1) 
   end
 dealer_cards
-prompt "Dealer has #{dealer_cards} and the sum is #{total(dealer_cards)}"
+prompt "Dealer has #{dealer_cards.sample(1)}"
 
-# break if player_cards.length + dealer_cards.length >= 52
+def player_busted?(cards)
+   total(cards) >= 21 
+end
 
-# player turn
+def dealer_busted?(cards)
+  total(cards) == 17
+end
 
 loop do
     prompt "Player, hit or stay?"
@@ -66,29 +65,33 @@ loop do
     if player_answer == "hit"
       player_cards << DECK.sample(1) + suits.sample(1)
       prompt "Your cards are now #{player_cards} and the sum is #{total(player_cards)}"
-   #   prompt "The dealer's cards are #{dealer_cards.sample(1)}"
-      if total(player_cards) >= 21
-        prompt "Game over."
+      if !!player_busted?(player_cards)
+        prompt "Player's a bust. Dealer wins."
         break
       end
     elsif player_answer == "stay"
       prompt "It's time for the dealer to play"
     end
   loop do
-      prompt "Dealer, hit or stay?"
-      dealer_answer = gets.chomp
-      if dealer_answer == "hit"
-        dealer_cards << DECK.sample(1) + suits.sample(1)
-        prompt "The dealer's cards are #{dealer_cards} and the sum is #{total(dealer_cards)}"
-        if total(dealer_cards) >= 17
-          prompt "Game over. Player wins"
-          break
-        end
+    prompt "Dealer, hit or stay?"
+    dealer_answer = gets.chomp
+    if dealer_answer == "hit"
+      dealer_cards << DECK.sample(1) + suits.sample(1)
+      if !!dealer_busted?(dealer_cards)
+        prompt "Dealer's a bust. Player wins"
         break
-      elsif dealer_answer == "stay"
-        prompt "Dealer stays"
+      end
+    elsif dealer_answer == "stay"
+      prompt "Dealer stays"
+      break
+    elsif dealer_answer == "stay" && player_answer == "stay"
+      if total(player_cards) > total(dealer_cards)
+        prompt "Player wins because its total #{total(player_cards)} is greater than dealer's total"
+      else
+        prompt "Dealer wins"
         break
       end
     end
+  end
   break if player_cards.length + dealer_cards.length == 52
 end
