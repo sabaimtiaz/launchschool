a70408c124a29d664773ff3e7e35a91afa741818
sabaimtiaz <saba.imtiaz@gmail.com>
2019-08-21 12:38:05 +0500

    Refactor twenty one

--- RB101/lesson_6/twentyone.rb
+++ RB101/lesson_6/twentyone.rb
@@ -58,7 +58,6 @@ def player_result(cards)
   end
 end
 
-
 def play_again?
   puts "-------------"
   prompt "Do you want to play again? (y or n)"
@@ -66,49 +65,60 @@ def play_again?
   answer.downcase.start_with?('y')
 end
 
-prompt "Welcome to Twenty One!"
-player_cards = []
-player_cards = generate(player_cards)
-prompt "You have #{player_cards[0][0]} and #{player_cards[1][0]}."
-
-dealer_cards = []
-dealer_cards = generate(dealer_cards)
-prompt "Dealer has #{dealer_cards[0][0]} and unknown card."
-player_answer = ''
 loop do
+  prompt "Welcome to Twenty One!"
+  player_cards = []
+  dealer_cards = []
+  2.times do
+    player_cards << DECK.sample(1) + SUITS.sample(1)
+    dealer_cards << DECK.sample(1) + SUITS.sample(1)
+  end
+
+  prompt "You have #{player_cards[0][0]} and #{player_cards[1][0]}."
+  prompt "Dealer has #{dealer_cards[0][0]} and unknown card."
+
   dealer_total = total(dealer_cards)
   player_total = total(player_cards)
-  loop do
-    prompt "Player, hit or stay?"
-    player_answer = gets.chomp
-    system "clear"
-    break if player_answer == "hit" || player_answer == "stay"
-    prompt "Please enter a correct option: hit or stay."
-  end
-  if player_answer == "hit"
-    player_cards << DECK.sample(1) + SUITS.sample(1)
-    prompt "You now have #{format_string(player_cards)}."
-    if !!player_busted?(player_cards)
-      player_result(player_cards)
-      break unless play_again?
-    end
-  elsif player_answer == "stay" && dealer_total <= 17
-    prompt "Dealer will play now..."
-    Kernel.sleep(1)
-    dealer_cards << DECK.sample(1) + SUITS.sample(1)
-    if !!dealer_busted?(dealer_cards)
-      dealer_result(dealer_cards)
-      break unless play_again?
-    end
-  elsif dealer_total > 17 && player_answer == "stay"
-    prompt "Dealer stayed."
-    if dealer_total > player_total
-      prompt "Dealer's total #{dealer_total} is higher."
-    else
-      prompt "Player's total is higher."
-    end
-    break
-  end
-end
-prompt "Thank you for playing Twenty One! Goodbye!"
 
+  loop do
+    player_answer = ''
+    loop do
+      prompt "Player, hit or stay?"
+      player_answer = gets.chomp
+      break if player_answer == "hit" || player_answer == "stay"
+      prompt "Please enter a correct option: hit or stay."
+    end
+    system "clear"
+    if player_answer == "hit"
+      player_cards << DECK.sample(1) + SUITS.sample(1)
+      prompt "You now have #{format_string(player_cards)}."
+    end
+    break if player_busted?(player_cards) || player_answer == "stay"
+  end
+
+  if player_busted?(player_cards)
+    player_result(player_cards)
+    play_again? ? next : break
+  else
+    prompt "You stayed at #{player_total}"
+  end
+
+  prompt "Dealer will play now..."
+  Kernel.sleep(1)
+  dealer_cards << DECK.sample(1) + SUITS.sample(1)
+  prompt "Dealer has #{format_string(dealer_cards)}."
+  if !!dealer_busted?(dealer_cards)
+    dealer_result(dealer_cards)
+    play_again? ? next : break
+  else
+    prompt "Dealer stays at #{dealer_total}."
+  end
+
+  prompt "Player has #{format_string(player_cards)}, equal to #{player_total}."
+  prompt "Dealer has #{format_string(dealer_cards)}, equal to #{dealer_total}."
+
+  break unless play_again?
+  system "clear"
+end
+
+prompt "Thank you for playing Twenty One! Good bye."
