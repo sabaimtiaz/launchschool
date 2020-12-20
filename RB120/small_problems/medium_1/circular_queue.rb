88a1cec18d520ee905b5eda041433eac2fbb7c0b
Saba Imtiaz <saba.imtiaz@gmail.com>
2020-12-20 08:49:21 +0500

    Adding changes and exercises for RB120

---
+++ RB120/small_problems/medium_1/circular_queue.rb
@@ -0,0 +1,55 @@
+class CircularQueue
+	attr_accessor :queue, :size
+	def initialize(size)
+		@queue = []
+		@size = size
+	end
+
+	def enqueue(item)
+		dequeue if queue.size == size
+		queue << item
+	end
+
+	def dequeue
+		queue.shift
+	end
+end
+
+queue = CircularQueue.new(3)
+puts queue.dequeue == nil
+
+queue.enqueue(1)
+queue.enqueue(2)
+puts queue.dequeue == 1
+
+queue.enqueue(3)
+queue.enqueue(4)
+puts queue.dequeue == 2
+
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+puts queue.dequeue == 5
+puts queue.dequeue == 6
+puts queue.dequeue == 7
+puts queue.dequeue == nil
+
+queue = CircularQueue.new(4)
+puts queue.dequeue == nil
+
+queue.enqueue(1)
+queue.enqueue(2)
+puts queue.dequeue == 1
+
+queue.enqueue(3)
+queue.enqueue(4)
+puts queue.dequeue == 2
+
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+puts queue.dequeue == 4
+puts queue.dequeue == 5
+puts queue.dequeue == 6
+puts queue.dequeue == 7
+puts queue.dequeue == nil
---
+++ RB120/small_problems/medium_1/fixed_array.rb
@@ -0,0 +1,67 @@
+class FixedArray
+	def initialize(size)
+		@array = Array.new(size)
+	end
+
+	def [](index)
+		@array.fetch(index)
+	end
+
+	def []=(index, value)
+		self[index]
+		@array[index] = value
+	end
+
+	def to_a
+		@array.clone
+	end
+
+	def to_s
+		to_a.to_s
+	end
+end
+
+fixed_array = FixedArray.new(5)
+puts fixed_array[3] == nil
+puts fixed_array.to_a == [nil] * 5
+
+fixed_array[3] = 'a'
+puts fixed_array[3] == 'a'
+puts fixed_array.to_a == [nil, nil, nil, 'a', nil]
+
+fixed_array[1] = 'b'
+puts fixed_array[1] == 'b'
+puts fixed_array.to_a == [nil, 'b', nil, 'a', nil]
+
+fixed_array[1] = 'c'
+puts fixed_array[1] == 'c'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', nil]
+
+fixed_array[4] = 'd'
+puts fixed_array[4] == 'd'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', 'd']
+puts fixed_array.to_s == '[nil, "c", nil, "a", "d"]'
+
+puts fixed_array[-1] == 'd'
+puts fixed_array[-4] == 'c'
+
+begin
+  fixed_array[6]
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[-7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
